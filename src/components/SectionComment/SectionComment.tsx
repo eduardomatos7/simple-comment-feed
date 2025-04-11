48afe8da26d8755bd17bc19561e74688d95dfcfc
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-10 23:11:14 -0300

    feat: cria área de comentários a partir de uma ação do enviar comentario

--- src/components/SectionComment/SectionComment.tsx
+++ src/components/SectionComment/SectionComment.tsx
@@ -1,22 +1,12 @@
 
+import Section from "../Section"
 function SectionComment() {
   return (
     <div className="container">
-    <div  className="section-comment">
-        <h2>Seção de comentários</h2>
-        <form action='' className="form-comment">
-        <div className="form-comment__input"> 
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' name="email"/> 
-        </div>
-        <div className="form-comment__input">
-        <label htmlFor='comment'>Comentário</label>
-        <textarea id='comment' name="comment" cols={50} rows={5}/>
-        </div> 
-        <button type="submit" className="btn-sbmt"> Enviar Comentário </button>
-        </form>
-        <div className="separator"/>
-    </div>
+      <div  className="section-comment">
+        <Section/>
+        
+      </div>
     </div>
   )
 }
