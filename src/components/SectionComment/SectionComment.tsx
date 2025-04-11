14f3d84cfa8ed1df8936110b6888ea76e3086f52
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-10 22:05:27 -0300

    style: estiliza e constroi container do feed de comentarios

--- src/components/SectionComment/SectionComment.tsx
+++ src/components/SectionComment/SectionComment.tsx
@@ -1,15 +1,22 @@
 
 function SectionComment() {
   return (
-    <div  className="container">
+    <div className="container">
+    <div  className="section-comment">
         <h2>Seção de comentários</h2>
-        <form action=''>
+        <form action='' className="form-comment">
+        <div className="form-comment__input"> 
         <label htmlFor='email'>Email</label>
         <input type='email' id='email' name="email"/> 
+        </div>
+        <div className="form-comment__input">
         <label htmlFor='comment'>Comentário</label>
-        <textarea id='comment' name="comment" cols={50} rows={5} style={{resize: 'none'}}/> 
-        <button type="submit"> Enviar Comentário </button>
+        <textarea id='comment' name="comment" cols={50} rows={5}/>
+        </div> 
+        <button type="submit" className="btn-sbmt"> Enviar Comentário </button>
         </form>
+        <div className="separator"/>
+    </div>
     </div>
   )
 }
